e5870ea68284e56d45f9ec1f3531d2f551398c97
Alexis2879 <giovanin007@gmail.com>
2024-06-25 00:26:03 -0500

    segundo commit

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -1,61 +1,105 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+require('dotenv').config
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
+const MockAdapter = require('@bot-whatsapp/database/mock')   /*comentar esta párte*/
+/*const { delay } = require('@whiskeysockets/baileys')*/
+const path = require('path')
+const fs = require('fs')
+const chat = require('./chatGPT')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+/*hacemos el llamado de la carpeta  mensajes*/
+const menuPath = path.join(__dirname, 'mensajes', 'menu.txt')
+const menu = fs.readFileSync(menuPath, 'utf8')
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+/*const flowVoice = addKeyword(EVENTS.VOICE_NOTE).addAnswer("Esta es una nota de voz", null, async (ctx, ctxFn) => {
+const text = await handlerAI(ctx)
+const prompt = promptConsultas
+const consulta = text
+const answer = await chat(prompt, consulta)
+await ctxFn.flowDynamic(answer.content)
+})  DESCOMENTAR   */
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowPrincipal = addKeyword(['hola', 'Buenos días', 'Buenas tardes', 'Buenas noches', 'hi'])
-    .addAnswer('🙌 Hola bienvenido a  *yokihero* 🔥' )
-    .addAnswer(
-        )
+/*mensaje de Bienvenida*/
+
+/*Menu del chat bor*/
+const flowMenuYoki = addKeyword(EVENTS.ACTION)
+    .addAnswer('🙌 este es el Menú', {
+        media:"https://sydney.pe/c/ropa-de-bebe/"
+})
+    
+
+
+/*Consultas del Chat bot*/
+const flowConsultas = addKeyword(EVENTS.ACTION)
+    .addAnswer('🙌 Consultame' )
+    .addAnswer('Haz tu Consulta', { capture: true }, async (ctx,ctxFn) => {
+        const prompt = 'Responde Hola' /*promptConsulta*/
+        const consulta = ctx.body
+        const answer = await chat(prompt, consulta)
+        console.log(answer.content)
+        /*await ctxFn.flowDynamic(answer.content)*/
+    })
+    
+
+const flowComprar = addKeyword(EVENTS.ACTION)
+    .addAnswer('🙌 Comprar: https://sydney.pe/c/ropa-de-bebe/' )
+    
+
+const flowWelcome = addKeyword(EVENTS.WELCOME)
+    .addAnswer("Este es el flujo Welcome", {
+        delay: 2000,
+        /*media: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtKQZd33bGyviMoji-g8X4lzQk9oJP5kIkhA&s"*/
+    },
+        async (ctx, ctxFn) => {
+            if (ctx.body.includes('casas')) {
+                await ctxFn.flowDynamic('Escirbiste casas')
+            } else {
+                await ctxFn.flowDynamic('Escribiste otra cosa')
+            }
+    })
+
+
+    /*Menu del Chatbot*/
+const menuFlow = addKeyword('Menu').addAnswer(
+    menu,
+    { capture: true },
+    async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
+        if (!['1', '2', '3',/* '4', '5',*/ '0'].includes(ctx.body)) { 
+            return fallBack(
+                'Respuesta No valida. Por favor, selecciona una opción válida',
+            );
+        }
+        switch (ctx.body) {
+            case '1':
+                return  gotoFlow(flowMenuYoki);
+            case '2':
+                return  gotoFlow(flowConsultas);
+            case '3':
+                return  gotoFlow(flowComprar);
+            /*case '4':
+                return await flowDynamic('Opción 4');
+            case '5':
+                return await flowDynamic('Opción 5');*/
+            case '0':
+                return await flowDynamic(
+                    'Saliendo... Puedes volver a acceder a este menú escribiendo '*menu*''
+                );
+        }
+    }
+);
+
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    /* const adapterDB = new MongoAdapter({
+        dbUri: process.env.MONGO_DB_URI,
+        dbName: "YoutubeTest"   */
+    const adapterFlow = createFlow([/*flowPrincipal,*/ flowWelcome, menuFlow, flowMenuYoki, flowConsultas, flowComprar]) // Incluyendo flowWelcome en el flujo
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
